45f3e67d26917fd7761fc707eb88d62432f1f20d
Gustavo <gustavoo.dcsilva@gmail.com>
2025-09-18 15:18:58 -0300

    Backup 1

---
+++ main/views/scripts/scripts.js
@@ -0,0 +1,15 @@
+
+VANTA.NET({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  points: 9.00,
+  maxDistance: 14.00,
+  spacing: 12.00
+})
+
